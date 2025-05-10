038b22d927b984596d5b8e0dbd37a06a00c3acc1
abhraneel bag <you@example.com>
2025-05-11 02:12:49 +0530

    fix: eslint set up

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config({
+  files: ['**/*.ts'],
+  extends: [
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+  ],
+  rules: {
+      "@/no-console": "error",
+      "@/quotes": ["error", "single"]
+  },
+});
