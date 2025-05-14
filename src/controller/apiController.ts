9aef1bef443818b915af310c957c399130ac0918
abhraneel bag <you@example.com>
2025-05-15 03:40:27 +0530

    feat: health-point

--- src/controller/apiController.ts
+++ src/controller/apiController.ts
@@ -2,11 +2,24 @@ import { NextFunction, Request, Response } from 'express';
 import httpResponse from '../util/httpResponse';
 import responeMessage from '../constants/responeMessage';
 import httpError from '../util/httpError';
+import quicker from '../util/quicker';
 
 export default {
     self: (req: Request, res: Response, next: NextFunction) => {
         try {
             httpResponse(req, res, 200, responeMessage.SUCCESS, { id: 1 });
+        } catch (err) {
+            httpError(next, err, req, 500);
+        }
+    },
+    health: (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const healthMessage = {
+                system: quicker.getSystemHealth(),
+                aplication: quicker.getApplicationHealth(),
+                timeStamp: Date.now()
+            };
+            httpResponse(req, res, 200, responeMessage.SUCCESS, healthMessage);
         } catch (err) {
             httpError(next, err, req, 500);
         }
